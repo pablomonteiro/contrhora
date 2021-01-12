26081976eef1a3e705bd36d1499c9aace243d726
Pablo <pablomf84@gmail.com>
2021-01-12 16:09:56 -0300

    Adjustment in migration replace all table name to lower case.

--- db/migrate/20201209195655_change_columns_requester_and_project.rb
+++ db/migrate/20201209195655_change_columns_requester_and_project.rb
@@ -1,11 +1,11 @@
 class ChangeColumnsRequesterAndProject < ActiveRecord::Migration[6.0]
   def self.up
     # Correction of projects names
-    execute 'UPDATE RECORDS SET project = \'VarejoFacil\' WHERE project = \'Varejofacil\''
-    execute 'UPDATE RECORDS SET project = \'Milênio\' WHERE project = \'Milënio\''
+    execute 'UPDATE records SET project = \'VarejoFacil\' WHERE project = \'Varejofacil\''
+    execute 'UPDATE records SET project = \'Milênio\' WHERE project = \'Milënio\''
     # Migrate values of requesters and projects to FK references
-    execute 'UPDATE RECORDS JOIN REQUESTERS RQ ON REQUESTER = RQ.NAME SET REQUESTER_ID = RQ.ID'
-    execute 'UPDATE RECORDS JOIN PROJECTS P ON PROJECT = P.NAME SET PROJECT_ID = P.ID'
+    execute 'UPDATE records JOIN requesters rq ON requester = rq.name SET requester_id = rq.id'
+    execute 'UPDATE records JOIN projects p ON project = p.name SET project_id = p.id'
   end
 
   def self.def down 
